88ec2afc59cd5b4a5cd3916c7867fffe1779fb0d
Duxdudu20 <126611962+Duxdudu20@users.noreply.github.com>
2023-11-09 10:47:07 +0200

    dieudonne BH test v0.1
    this repostory don't include node_module remember to install it while you pull it to work in local

---
+++ src/component/form.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class navabar extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <>
+            <form>
+                <input></input>
+            </form>
+            </>
+        );
+    }
+}
+ 
+export default navabar;
